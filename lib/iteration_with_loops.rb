4bfff97edb1f9996f31eeeabcaaef71c760b2e51
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-10 05:44:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def find_min_in_nested_arrays(src)
   
 outer_results = []
 row_index = 0
-while row_index < src do
+while row_index < src.count do
   element_index = 0
   min = 0
   while element_index < src[row_index].count do
