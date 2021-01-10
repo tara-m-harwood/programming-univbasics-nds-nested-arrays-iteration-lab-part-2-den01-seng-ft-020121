cba63638950930f885168e56b639dcc0a633af26
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-10 05:46:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,9 +8,9 @@ outer_results = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  min = 0
+  min = src[row_index][element_index]
   while element_index < src[row_index].count do
-    num = src[row_index][element_index].to_int
+    num = src[row_index][element_index]
     if num < min
       min = src[row_index][element_index]
     end
